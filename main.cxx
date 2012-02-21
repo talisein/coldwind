91370b22a3f1742dc0dcdd5b60c9ebb47652f436
Andrew Potter <agpotter@gmail.com>
2012-02-20 18:25:59 -0800

    Start to add some error checking around curl

--- main.cxx
+++ main.cxx
@@ -3,11 +3,16 @@
 #include <glibmm/thread.h>
 #include "application.hxx"
 #include <curl/curl.h>
-
+#include <iostream>
 
 int main (int argc, char *argv[])
 {
-  curl_global_init(CURL_GLOBAL_ALL);
+  CURLcode code = curl_global_init(CURL_GLOBAL_ALL);
+  if (code != CURLE_OK) {
+    std::cerr << "Error: While initializing curl: " << curl_easy_strerror(code) << std::endl;
+    return EXIT_FAILURE;
+  }
+
   if(!Glib::thread_supported()) Glib::thread_init();
 
   Derp::Application app(argc, argv);
