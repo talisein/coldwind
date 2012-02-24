8e2d185c9e5158b26f60ccf0fa1fd1aa26475933
Andrew Potter <agpotter@gmail.com>
2012-02-24 14:09:44 -0800

    Import checkboxes into pointers

--- application.cxx
+++ application.cxx
@@ -7,12 +7,14 @@
 #include "config.h"
 
 Derp::Application::Application(int argc, char*argv[]) : 
+  LURKER_TIMEOUT_SECS(60),
   num_downloading(0.0),
   num_downloaded(0.0),
   m_kit(argc, argv)
 {
   try {
     auto refBuilder = Gtk::Builder::create_from_file(COLDWIND_GLADE_LOCATION);
+
     refBuilder->get_widget("mainWindow", m_window);
     refBuilder->get_widget("urlEntry", m_urlEntry);
     refBuilder->get_widget("goToggle", m_goButton);
@@ -20,25 +22,29 @@ Derp::Application::Application(int argc, char*argv[]) :
     refBuilder->get_widget("lurkSpinButton", m_lurkSpinButton);
     refBuilder->get_widget("fileChooserButton", m_fileChooserButton);
     refBuilder->get_widget("killMeImage", m_image);
+    refBuilder->get_widget("boardDirCheckbox", m_boardDirCheckbox);
+    refBuilder->get_widget("threadDirCheckbox", m_threadDirCheckbox);
+    refBuilder->get_widget("originalFilenameCheckbox", m_originalFilenameCheckbox);
+
     m_lurkAdjustment = Glib::RefPtr<Gtk::Adjustment>::cast_static(refBuilder->get_object("lurkAdjustment"));
     m_xAdjustment = Glib::RefPtr<Gtk::Adjustment>::cast_static(refBuilder->get_object("xAdjustment"));
     m_yAdjustment = Glib::RefPtr<Gtk::Adjustment>::cast_static(refBuilder->get_object("yAdjustment"));
+
     m_killmegif = Gdk::PixbufAnimation::create_from_file(COLDWIND_KILLMEGIF_LOCATION);
     m_fangpng = Gdk::Pixbuf::create_from_file(COLDWIND_FANGPNG_LOCATION);
     m_errorgif = Gdk::PixbufAnimation::create_from_file(COLDWIND_ERRORGIF_LOCATION);
-
-    m_image->set(m_fangpng);
-
   } catch (const Glib::FileError& ex) {
     std::cerr << "FileError: " << ex.what() << std::endl;
-    exit(1);
+    exit(EXIT_FAILURE);
   } catch (const Gtk::BuilderError& ex) {
     std::cerr << "BuilderError: " << ex.what() << std::endl;
-    exit(1);
+    exit(EXIT_FAILURE);
   } catch (const Glib::MarkupError& ex) {
     std::cerr << "MarkupError: " << ex.what() << std::endl;
-    exit(1);
+    exit(EXIT_FAILURE);
   }
+  
+  m_image->set(m_fangpng);
 
   m_goButton->signal_toggled().connect( sigc::mem_fun(*this, &Derp::Application::signal_go));
   m_manager.signal_download_finished.connect(sigc::mem_fun(*this, &Derp::Application::download_finished));
@@ -46,7 +52,7 @@ Derp::Application::Application(int argc, char*argv[]) :
   m_manager.signal_all_downloads_finished.connect(sigc::mem_fun(*this, &Derp::Application::downloads_finished));
   m_manager.signal_starting_downloads.connect(sigc::mem_fun(*this, &Derp::Application::starting_downloads));
   
-  Glib::signal_timeout().connect_seconds(sigc::bind_return(sigc::mem_fun(m_lurker, &Derp::Lurker::run), true), 60);
+  Glib::signal_timeout().connect_seconds(sigc::bind_return(sigc::mem_fun(m_lurker, &Derp::Lurker::run), true), LURKER_TIMEOUT_SECS);
 }
 
 void Derp::Application::run() {
--- application.hxx
+++ application.hxx
@@ -21,6 +21,8 @@ namespace Derp {
     void run();
 
   private:
+    const int LURKER_TIMEOUT_SECS;
+
     void signal_go();
     void download_finished();
     void downloads_finished(int num, const Request& request);
@@ -33,23 +35,27 @@ namespace Derp {
     int num_download_errors;
 
     Gtk::Main m_kit;
-    Gtk::Window* m_window;
-    Gtk::Image* m_image;
-    Glib::RefPtr<Gdk::PixbufAnimation> m_killmegif;
-    Glib::RefPtr<Gdk::PixbufAnimation> m_errorgif;
-    Glib::RefPtr<Gdk::Pixbuf> m_fangpng;
-    Gtk::ProgressBar* m_progressBar;
-    Gtk::ToggleButton* m_goButton;
-    Glib::RefPtr<Gtk::Adjustment> m_lurkAdjustment;
-    Glib::RefPtr<Gtk::Adjustment> m_xAdjustment;
-    Glib::RefPtr<Gtk::Adjustment> m_yAdjustment;
-    Gtk::SpinButton* m_lurkSpinButton;
-    Gtk::Entry* m_urlEntry;
-    Gtk::FileChooserButton* m_fileChooserButton;
 
     Glib::Timer m_timer;
     Derp::Manager m_manager;
     Derp::Lurker m_lurker;
+
+    Gtk::Window* m_window;
+    Gtk::Image* m_image;
+    Gtk::ProgressBar* m_progressBar;
+    Gtk::ToggleButton* m_goButton;
+    Gtk::SpinButton* m_lurkSpinButton;
+    Gtk::Entry* m_urlEntry;
+    Gtk::FileChooserButton* m_fileChooserButton;
+    Gtk::CheckButton* m_boardDirCheckbox;
+    Gtk::CheckButton* m_threadDirCheckbox;
+    Gtk::CheckButton* m_originalFilenameCheckbox;
+    Glib::RefPtr<Gdk::PixbufAnimation> m_killmegif;
+    Glib::RefPtr<Gdk::PixbufAnimation> m_errorgif;
+    Glib::RefPtr<Gdk::Pixbuf> m_fangpng;
+    Glib::RefPtr<Gtk::Adjustment> m_lurkAdjustment;
+    Glib::RefPtr<Gtk::Adjustment> m_xAdjustment;
+    Glib::RefPtr<Gtk::Adjustment> m_yAdjustment;
   };
 }
 #endif
