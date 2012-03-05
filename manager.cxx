be774e378277b7755c3799961202706435b65780
Andrew Potter <agpotter@gmail.com>
2012-03-05 15:40:24 -0800

    Change Derp::Manager::Error::ERROR to Error::CURL_ERROR because of mingw collision

--- manager.cxx
+++ manager.cxx
@@ -1,74 +1,74 @@
 #include "manager.hxx"
 
 Derp::Manager::Manager() : 
-  is_working(false)
+	is_working(false)
 {
-  m_parser.signal_parsing_finished.connect(sigc::mem_fun(*this, &Derp::Manager::parsing_finished));
-  m_parser.signal_thread_404.connect(sigc::mem_fun(*this, &Derp::Manager::thread_404));
-  m_hasher.signal_hashing_finished.connect(sigc::mem_fun(*this, &Derp::Manager::hashing_finished));
-  m_downloader.signal_download_finished.connect(sigc::mem_fun(*this, &Derp::Manager::download_finished));
-  m_downloader.signal_download_error.connect(sigc::mem_fun(*this, &Derp::Manager::download_error));
+	m_parser.signal_parsing_finished.connect(sigc::mem_fun(*this, &Derp::Manager::parsing_finished));
+	m_parser.signal_thread_404.connect(sigc::mem_fun(*this, &Derp::Manager::thread_404));
+	m_hasher.signal_hashing_finished.connect(sigc::mem_fun(*this, &Derp::Manager::hashing_finished));
+	m_downloader.signal_download_finished.connect(sigc::mem_fun(*this, &Derp::Manager::download_finished));
+	m_downloader.signal_download_error.connect(sigc::mem_fun(*this, &Derp::Manager::download_error));
 }
 
 bool Derp::Manager::download_async(const Derp::Request& data) {
-  if (!is_working) {
-    is_working = true;
-    m_request = data;
-    is_hashing = is_parsing = true;
-    m_parser.parse_async(m_request);
-    m_hasher.hash_async(m_request);
-    return true;
-  } else {
-    return false; 
- }
+	if (!is_working) {
+		is_working = true;
+		m_request = data;
+		is_hashing = is_parsing = true;
+		m_parser.parse_async(m_request);
+		m_hasher.hash_async(m_request);
+		return true;
+	} else {
+		return false; 
+	}
 }
 
 void Derp::Manager::parsing_finished() {
-  is_parsing = false;
-  try_download();
+	is_parsing = false;
+	try_download();
 }
 
 void Derp::Manager::hashing_finished() {
-  is_hashing = false;
-  try_download();
+	is_hashing = false;
+	try_download();
 }
 
 void Derp::Manager::thread_404() {
-  is_working = false;
-  signal_download_error(Derp::Error::THREAD_404);
-  // TODO: Need to manage state better
+	is_working = false;
+	signal_download_error(Derp::Error::THREAD_404);
+	// TODO: Need to manage state better
 }
 
 void Derp::Manager::try_download() {
-  if ( !(is_parsing || is_hashing) ) {
-    num_errors = 0;
-    num_downloaded = 0;
-    num_downloading = m_parser.request_downloads(m_downloader, m_hasher, m_request);
-    if ( num_downloading == 0 ) {
-      done();
-    } else {
-      signal_starting_downloads(num_downloading);
-    }
-  }
+	if ( !(is_parsing || is_hashing) ) {
+		num_errors = 0;
+		num_downloaded = 0;
+		num_downloading = m_parser.request_downloads(m_downloader, m_hasher, m_request);
+		if ( num_downloading == 0 ) {
+			done();
+		} else {
+			signal_starting_downloads(num_downloading);
+		}
+	}
 }
 
 void Derp::Manager::download_finished() {
-  num_downloaded++;
-  signal_download_finished();
-  if (num_downloading == (num_downloaded + num_errors)) {
-    done();
-  }
+	num_downloaded++;
+	signal_download_finished();
+	if (num_downloading == (num_downloaded + num_errors)) {
+		done();
+	}
 }
 
 void Derp::Manager::download_error() {
-  num_errors++;
-  signal_download_error(Derp::Error::ERROR);
-  if (num_downloading == (num_downloaded + num_errors)) {
-    done();
-  }
+	num_errors++;
+	signal_download_error(Derp::Error::CURL_ERROR);
+	if (num_downloading == (num_downloaded + num_errors)) {
+		done();
+	}
 }
 
 void Derp::Manager::done() {
-  is_working = false;
-  signal_all_downloads_finished(num_downloaded, m_request);
+	is_working = false;
+	signal_all_downloads_finished(num_downloaded, m_request);
 }
--- manager.hxx
+++ manager.hxx
@@ -9,43 +9,43 @@
 
 namespace Derp {
   
-  enum Error {
-    THREAD_404,
-    DUPLICATE_FILE,
-    ERROR
-  };
+	enum Error {
+		THREAD_404,
+		DUPLICATE_FILE,
+		CURL_ERROR
+	};
 
-  class Manager {
-  public:
-    Manager();
+	class Manager {
+	public:
+		Manager();
 
-    bool download_async(const Derp::Request& data);
-    sigc::signal<void, int, const Derp::Request&> signal_all_downloads_finished;
-    sigc::signal<void> signal_download_finished;
-    sigc::signal<void, int> signal_starting_downloads;
-    sigc::signal<void, const Derp::Error&> signal_download_error;
+		bool download_async(const Derp::Request& data);
+		sigc::signal<void, int, const Derp::Request&> signal_all_downloads_finished;
+		sigc::signal<void> signal_download_finished;
+		sigc::signal<void, int> signal_starting_downloads;
+		sigc::signal<void, const Derp::Error&> signal_download_error;
 
-	  double getProgress() { return m_downloader.getProgress(); };
-  private:
+		double getProgress() { return m_downloader.getProgress(); };
+	private:
 
-    void parsing_finished();
-    void hashing_finished();
-    void try_download();
-    void download_finished();
-    void download_error();
-    void done();
-    void thread_404();
-    bool is_working;
-    bool is_hashing, is_parsing;
-    int num_downloading, num_downloaded, num_errors;
+		void parsing_finished();
+		void hashing_finished();
+		void try_download();
+		void download_finished();
+		void download_error();
+		void done();
+		void thread_404();
+		bool is_working;
+		bool is_hashing, is_parsing;
+		int num_downloading, num_downloaded, num_errors;
 
-    Derp::Parser m_parser;
-    Derp::Hasher m_hasher;
-    Derp::Downloader m_downloader;
+		Derp::Parser m_parser;
+		Derp::Hasher m_hasher;
+		Derp::Downloader m_downloader;
 
-    Derp::Request m_request;
+		Derp::Request m_request;
 
-  };
+	};
 }
 
 #endif
